d34b712d7334b3413617a2d1b5d692b619e36eaa
christiankane42 <christiankane@tamu.edu>
2021-09-21 10:03:48 -0500

    editing new.html.erb - attempting to add new input fields

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -4,11 +4,15 @@ require 'rails_helper'
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
-    fill_in 'Title', with: 'harry potter'
+    fill_in 'title', with: 'harry potter'
+    fill_in 'author', with: 'JK Rowling'
+    fill_in 'price', with: 10.00
+    # 1997 June 26
+    fill_in 'published_date', with: '1997-06-26'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
   end
 
-  
+
 end
