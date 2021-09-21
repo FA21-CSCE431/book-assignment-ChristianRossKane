a23933d859be6716bf1bb99058381a94a7fc34bb
christiankane42 <christiankane@tamu.edu>
2021-09-21 10:08:32 -0500

    editing new.html.erb - attempting to add new input fields - 2

--- spec/views/books/new.html.erb_spec.rb
+++ spec/views/books/new.html.erb_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe "books/new", type: :view do
   before(:each) do
     assign(:book, Book.new(
-      title: "MyString"
+      title: "MyString",
+      author: "MyString",
+      price: 10.00,
+      published_date: '1997-06-26'
     ))
   end
 
