e6f7683bcf37d494521c58e7cae7979a16c700c2
christiankane42 <christiankane@tamu.edu>
2021-09-28 14:20:26 -0500

    adding final product to test branch

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -1,18 +1,56 @@
 # location: spec/feature/integration_spec.rb
 require 'rails_helper'
 
-RSpec.describe 'Creating a book', type: :feature do
-  scenario 'valid inputs' do
+RSpec.describe 'Testing book features', type: :feature do
+
+  scenario 'valid input - title' do
     visit new_book_path
-    fill_in 'title', with: 'harry potter'
-    fill_in 'author', with: 'JK Rowling'
-    fill_in 'price', with: 10.00
+    fill_in 'book[title]', with: 'harry potter'
+    fill_in 'book[author]', with: 'JK Rowling'
+    fill_in 'book[price]', with: 10.00
     # 1997 June 26
-    fill_in 'published_date', with: '1997-06-26'
+    fill_in 'book[published_date]', with: '1997-06-26'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
   end
+
+  scenario 'valid input - author' do
+    visit new_book_path
+    fill_in 'book[title]', with: 'harry potter'
+    fill_in 'book[author]', with: 'JK Rowling'
+    fill_in 'book[price]', with: 10.00
+    # 1997 June 26
+    fill_in 'book[published_date]', with: '1997-06-26'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('JK Rowling')
+  end
+
+  scenario 'valid input - price' do
+    visit new_book_path
+    fill_in 'book[title]', with: 'harry potter'
+    fill_in 'book[author]', with: 'JK Rowling'
+    fill_in 'book[price]', with: 10.00
+    # 1997 June 26
+    fill_in 'book[published_date]', with: '1997-06-26'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content(10.00)
+  end
+
+  scenario 'valid input - date' do
+    visit new_book_path
+    fill_in 'book[title]', with: 'harry potter'
+    fill_in 'book[author]', with: 'JK Rowling'
+    fill_in 'book[price]', with: 10.00
+    # 1997 June 26
+    fill_in 'book[published_date]', with: '1997-06-26'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('1997-06-26')
+  end
+
 
 
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -10,29 +10,29 @@ RSpec.describe Book, type: :model do
     expect(subject).to be_valid
   end
 
-  # name = string
-  # it 'is not valid without a name' do
-  #   subject.title = nil
-  #   expect(subject).not_to be_valid
-  # end
-  #
-  # # author = string
-  # it 'is not valid without an author' do
-  #   subject.author = nil
-  #   expect(subject).not_to be_valid
-  # end
-  #
-  # # price = decimal
-  # it 'is not valid without a price' do
-  #   subject.price = nil
-  #   expect(subject).not_to be_valid
-  # end
-  #
-  # # published_date = date (date is a datatype, like in sql)
-  # it 'is not valid without a published_date' do
-  #   subject.published_date = nil
-  #   expect(subject).not_to be_valid
-  # end
+  #name = string
+  it 'is not valid without a name' do
+    subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  # author = string
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  # price = decimal
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  # published_date = date (date is a datatype, like in sql)
+  it 'is not valid without a published_date' do
+    subject.published_date = nil
+    expect(subject).not_to be_valid
+  end
 
 
 end
